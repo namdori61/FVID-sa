000750221c045f8ed2f17531a619f992350de92e
Sangho.Nam <namdori61@gmail.com>
2019-07-26 14:14:41 +0900

    Fix: scheduler from airflow to celery

--- controller.py
+++ controller.py
@@ -44,7 +44,31 @@ class Controller():
         self.logic_manager = LogicManager()
         self.db_manager = DataBaseManager()
 
-if __name__ == "__main__":
+# Scheduler
+from celery.schedules import crontab
+from celery import Celery
+app = Celery('crawler', backend='redis://localhost:6379/0', broker='redis://localhost:6379/0')
+
+app.conf.update(
+    BROKER_URL='redis://localhost:6379/0',
+    CELERY_TASK_SERIALIZER='json',
+    CELERY_ACCEPT_CONTENT=['json'],  # Ignore other content
+    CELERY_RESULT_SERIALIZER='json',
+    CELERY_TIMEZONE='Asia/Seoul',
+    CELERY_ENABLE_UTC=False,
+    CELERYBEAT_SCHEDULE = {
+        'get_keyword_data-every-day': {
+            'task': 'controller.get_keyword_data',
+            'schedule': crontab(hour=15),
+            'args': ()
+        },
+    }
+)
+
+@app.task
+
+def get_keyword_data():
+
     controller = Controller()
 
     pool = controller.db_manager.create_pool(controller.db_host, controller.db_port, controller.db_db, controller.db_user, controller.db_pw)
@@ -58,5 +82,5 @@ if __name__ == "__main__":
         refined_data = controller.logic_manager.keyword_data_transformer(lab_data, ad_data)
         controller.db_manager.insert_data(pool, controller.db_trend_table, refined_data)
         controller.crawler.time_sleep(10)
-    
+
     controller.db_manager.closer(pool)
